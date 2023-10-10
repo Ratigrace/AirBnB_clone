938d94d2c504e3f1f45d17de3933f5d831832a33
Ratigrace <chijongwegrace@gmail.com>
2023-10-10 12:28:01 -0700

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''defining the BaseModel class'''
+
+    def __init__(self, *args, **kwargs):
+        '''Initializing public instance attributes.
+        Args:
+            *args: unused
+            **kwargs: (dict) of attributes
+        '''
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''returns a string representation of BaseModel'''
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        '''updates current attributes with date and time'''
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''returns a dictionary containg keys/values for instances'''
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
